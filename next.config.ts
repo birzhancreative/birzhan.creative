a3aacc8c25e418ec3d9719b00c810c383b354d46
Birzhan <birzhan.creative@gmail.com>
2026-04-27 09:11:30 +0200

    fix: exclude public folder from output file tracing to fix Vercel limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'public/**/*',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
